50d00b6a71fac8888ca51f67333b30a2f2270f93
Gargtanisha11 <guptatanisha185@gmail.com>
2023-08-24 09:26:55 -0700

    Update

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
-// react element 
-// this is react eleement using react 
-// what is going on below 
-// React.createlement => react object => root.render => html element 
-const heading = React.createElement("h1",
- { id:"heading"},
-  " hello i am h1 tag ");
 
- // console.log(heading);
+// react element
+// this is react eleement using react
+// what is going on below
+// React.createlement => react object => root.render => html element
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  " hello i am h1 tag "
+);
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(heading);
+// console.log(heading);
 
-  // JSX
-  // react eleement using jsx 
-  const jsxheading = <h1 id="jsxheading" > {1+2} hello i am h1 tag using JSX </h1>
-  root.render(jsxheading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// JSX
+// react eleement using jsx
+
+
+const Jsxheading2 = () => <h1 className="jsxheading2"> hello i am h1 tag using JSX</h1>
+
+
+const Jsxheading3 = () => (
+  <h1 className="jsxheading3"> hello i am h1 tag using JSX</h1>
+);
+
+const Jsxheading = () => {
+  return (
+    <h1 className="jsxheading">
+      hello i am h1 tag using JSX
+    </h1>
+  );
+};
+const head = <h2 id="head "> hye i am h2 tag<Jsxheading/> </h2>;
+
+root.render(head);
